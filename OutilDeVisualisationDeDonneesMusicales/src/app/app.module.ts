a1dc09ff810dc0c8a562761779c47d8c5882b81e
Maxime M <maxime.moineau@free.fr>
2019-10-25 10:18:12 +0200

    add basic search bar no fetch

--- OutilDeVisualisationDeDonneesMusicales/src/app/app.module.ts
+++ OutilDeVisualisationDeDonneesMusicales/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { DisplayTableComponent } from './display-table/display-table.component';
+import { DisplayDetailsComponent } from './display-details/display-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    DisplayTableComponent,
+    DisplayDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
