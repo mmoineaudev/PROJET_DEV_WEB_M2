24290c25e1b9fd6114fa42a88a08e6e610b42728
Maxime M <maxime.moineau@free.fr>
2019-11-12 16:30:38 +0100

    On va repartir de là c'est moins galère

--- projet-mmoineau/src/App.js
+++ projet-mmoineau/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import './components/artists';
+import Artists from './components/artists';
 function App() {
   return (
     <div className="App">
-      <artists></artists>
+      <Artists></Artists>
     </div>
   );
 }
--- projet-mmoineau/src/components/artists/artists.js
+++ projet-mmoineau/src/components/artists/artists.js
@@ -1,24 +1,14 @@
-import React from 'react';
+import {React, Component} from 'react';
 import PropTypes from 'prop-types';
-import styles from './artists.scss';
-
-const artists = props => (
-	<div>This is a component called artists.</div>
-);
-
-// todo: Unless you need to use lifecycle methods or local state,
-// write your component in functional form as above and delete
-// this section. 
-// class artists extends React.Component {
-//   render() {
-//     return <div>This is a component called artists.</div>;
-//   }
-// }
-
-const artistsPropTypes = {
-	// always use prop types!
-};
-
-artists.propTypes = artistsPropTypes;
-
-export default artists;
+import styles from './artists.css';
+class Artists extends Component {
+	constructor() {
+		super();
+	}
+	
+	render() {
+		return "hello";
+	}
+	
+}
+export default Artists;
