704f41e682c3c92b6cad2cc30826dfe2ec5b3d4f
Maxime M <maxime.moineau@free.fr>
2019-12-10 16:10:47 +0100

    galere sur les promises pour le premier fetch

--- projet-mmoineau/src/components/mainPage/mainPage.js
+++ projet-mmoineau/src/components/mainPage/mainPage.js
@@ -7,25 +7,43 @@ class MainPage extends React.Component {
 
 	constructor() {
 	  super();
-	  
+	  this.state = { start: 0 }
+
 	}
 
 	fetchArtists(){
-		
+		const URL = "https://wasabi.i3s.unice.fr/api/v1/artist_all/"+this.state.start ;
+		var headers = new Headers();
+		var requestInfos = { method: 'GET',
+					headers: headers,
+					mode: 'cors',
+					cache: 'default' };
+		const result = fetch(URL,requestInfos).then((response) => {
+			if(response.ok){
+				return response.body ;
+			}else{
+				return response.statusText ;				
+			}
+		});
+		console.log(URL, result);
+		return result.then((e) => console.log(e)  );
 	}
 
 	render() {
-	  return (
-	  <div>
+	const artists = this.fetchArtists();
+	console.log(artists);
+
+	return (
+	 <div>
 		<div>TITLE</div>
 		<div>SEARCHBAR</div>
 		<div><Artists /><Artists /><Artists /></div>
 		<div>TAGS</div>
 	  </div>
-	  );
+	  );  
+	
+
 	}
-  
 }
-  
 
 export default MainPage;
