3d6c71c1f0a063fedd5b5d59c22911b54925d7db
Max M <maxime.moineau.dev@gmail.com>
2020-01-14 22:35:10 +0100

    le fetch passe

--- projet-mmoineau/src/components/artists/artists.js
+++ projet-mmoineau/src/components/artists/artists.js
@@ -1,6 +1,5 @@
 import {React, Component} from 'react';
 import PropTypes from 'prop-types';
-import styles from './artists.css';
 class Artists extends Component {
 	constructor() {
 		super();
--- projet-mmoineau/src/components/mainPage/mainPage.js
+++ projet-mmoineau/src/components/mainPage/mainPage.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Artists from '../artists';
-import styles from './mainPage.scss';
+import React, { Component } from 'react'
+
+import PropTypes from 'prop-types'
+import Artists from '../artists'
 
 class MainPage extends React.Component {
 
 	constructor() {
-	  super();
+	  super()
 	  this.state = { 
 			start: 0 ,
 			artists: []
@@ -14,36 +14,59 @@ class MainPage extends React.Component {
 	}
 
 	async fetchArtists(){
-		const URL = "https://wasabi.i3s.unice.fr/api/v1/artist_all/"+this.state.start ;
-		const headers = new Headers();
+		console.log('fetchArtists')
+		const URL = "https://wasabi.i3s.unice.fr/api/v1/artist_all/"+this.state.start 
+		const headers = new Headers()
 		const requestInfos = { method: 'GET',
 					headers: headers,
 					mode: 'cors',
-					cache: 'default' };
-		const result = await fetch(URL,requestInfos);
-		const jsonRes = await result.json();
-		return jsonRes;
+					cache: 'default' }
+		const result = await fetch(URL,requestInfos)
+		//console.log('fetchArtists ', result)
+		const jsonRes = await result.json()
+		//console.log('fetchArtists ', jsonRes)
+		return jsonRes
 	}
 	
 	componentWillMount() {
-		const artists = this.fetchArtists().then((e) => {
-			//this.setState({artists: });
-			console.log(e)
-		});
+		console.log('componentWillMount')
+		this.fetchArtists()
+			  .then(newArtist => { // data c'est le texte json de response ci-dessus
+				this.setState({
+					artists: newArtist
+			   })
+			  }).catch(err => {
+				console.log("erreur dans le get : " + err)
+			  }) 
 	}
 	
+	getFormattedArtistsFromState() {
+		console.log('getFormattedArtistsFromState')
+		let formattedArtist = []
+
+		if(this.state.artists.length>0) this.state.artists.forEach(element => {
+			//console.log(element)
+			formattedArtist.push(element.name)
+		})
+		if(formattedArtist.length>0) formattedArtist.forEach(element => {
+			element= <li> element </li>
+		})
+		return (formattedArtist)?<ul>{formattedArtist.join()}</ul>:<p>pas d'artiste...</p> 
+	}
+
 	render() {
-	return (
-	 <div>
-		<div>TITLE</div>
-		<div>SEARCHBAR</div>
-		<div>{this.state.artists.}</div>
-		<div>TAGS</div>
-	  </div>
-	  );  
+		const artists = this.getFormattedArtistsFromState()
+		return (
+	 	<div>
+			<div>TITLE</div>
+			<div>SEARCHBAR</div>
+			<div>{artists}</div>
+			<div>TAGS</div>
+	  	</div>
+	  )  
 	
 
 	}
 }
 
-export default MainPage;
+export default MainPage
