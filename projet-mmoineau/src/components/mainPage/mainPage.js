00bcd7601915b8e5ddf44f0cbdbd324a62fe7c34
Votre Nom <Vous@exemple.com>
2020-01-18 21:46:38 +0100

    au départ ça parait pas naturel et compliqué, au fur et a mesure ça empire

--- projet-mmoineau/src/components/mainPage/mainPage.js
+++ projet-mmoineau/src/components/mainPage/mainPage.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, Input } from 'react'
 
 import PropTypes from 'prop-types'
 import Artists from '../artists'
+import artists from '../artists'
 
 class MainPage extends React.Component {
-
 	constructor() {
+	  console.log(`#############################################################
+DEBUG : constructor
+#############################################################`)
 	  super()
 	  this.state = { 
 			start: 0 ,
-			artists: []
+			search : 'An artist',
+			display: 'all', //'all', 'search'
+			artists: [],
+			htmlArtists: []
 		}
 	}
-
+	getAll(){
+		console.log('getAll')
+		this.setState({display:'all'})
+	}
 	async fetchArtists(){
-		console.log('fetchArtists')
+		console.log(`#############################################################
+DEBUG : fetchArtists
+############################################################`)
 		const URL = "https://wasabi.i3s.unice.fr/api/v1/artist_all/"+this.state.start 
 		const headers = new Headers()
 		const requestInfos = { method: 'GET',
@@ -22,89 +33,92 @@ class MainPage extends React.Component {
 					mode: 'cors',
 					cache: 'default' }
 		const result = await fetch(URL,requestInfos)
-		console.log('fetchArtists ', URL , result)
 		const jsonRes = await result.json()
-		console.log('fetchArtists ', jsonRes)
-		return jsonRes
-	}
+		console.log(`#############################################################
+		DEBUG : fetchArtists
+DEBUG : `,jsonRes,`
+############################################################`)
+		this.setState({
+			artists: Promise.resolve(jsonRes),
+			display: 'all'
+		})
+	}	
 
+	getSearch(){
+		console.log('getSearch : ', this.search.value)
+		this.setState({display:'search',
+						searche: this.search.value})
+	}
 	//https://wasabi.i3s.unice.fr/search/member/name/:memberName
 	async fetchArtistsByName(){
-		console.log('fetchArtistsByName')
-		const URL = "https://wasabi.i3s.unice.fr/search/member/name/"+this.state.searchbar 
+		console.log(`#############################################################
+DEBUG : fetchArtistsByName `, this.search.value, `
+############################################################`)
+		const URL = "https://wasabi.i3s.unice.fr/search/member/name/"+this.state.search
 		const headers = new Headers()
 		const requestInfos = { method: 'GET',
 					headers: headers,
 					mode: 'cors',
 					cache: 'default' }
 		const result = await fetch(URL,requestInfos)
-		//console.log('fetchArtistsByName ', result)
 		const jsonRes = await result.json()
 		console.log('fetchArtistsByName ', jsonRes)
-		return jsonRes
-	}
-	getAllArtist(){
-		console.log('this.getAllArtist')
-		this.fetchArtists().then(newArtist => {
-			this.setState({
-				artists: newArtist
-			})
+		this.setState({
+			artists: Promise.resolve(jsonRes),
+			display: 'search'
 		})
-
 	}
-	getArtistFromSearch(){
-		console.log('this.getArtistFromSearch')
-		this.fetchArtistsByName().then(newArtist => {
-			this.setState({
-				artists: newArtist
-			})
-		})
 
-	}
-	componentWillMount() {
-		console.log('componentWillMount');
-		let newArtist = [];
-		if( (this.state.artists && this.state.artists.length>0) ){
-			//ok
-		}else {
-			if(this.props.search && this.props.search.length>0){
-				newArtist = this.getArtistFromSearch();
-			}else{
-				newArtist = this.getAllArtist();
-			}
-			this.setState({
-				artists: newArtist
-			})	
+
+	componentDidMount() {
+		console.log(`#############################################################
+START : componentWillMount`, this.state, this.props, 
+`############################################################`)
+		let newArtists = []
+		switch (this.state.display) {
+			case 'all':
+				this.displayFactory(Promise.resolve(this.fetchArtists()))
+				break;
+			case 'search':
+				this.displayFactory(Promise.resolve(this.fetchArtistsByName()))
+				break;
+			default:
+				console.log('default display')
+				break;
 		}
-		console.log('componentWillMount', this.state, this.props)
+		console.log(`#############################################################
+END : componentWillMount`, this.state, this.props, 
+`############################################################`)
 	}
-	
-	getFormattedArtistsFromState() {
-		console.log('getFormattedArtistsFromState')
-		if(this.state.artists){
-			const artists = this.state.artists;
-			const res = (this.state.artists.length>0)?
-			<ul>{artists.map((element) => 
-				<li key={element._id}>{'*' + element.name +' ; '+ element.type,element.genres}</li>
-			)}</ul> : <p> sadness, pas d'artistes </p>; 
-			console.log(res)
-			
-			return res
-		} return [];	
+
+	displayFactory(artists){
+		console.log(`#############################################################
+DEBUG : displayFactory
+############################################################`)
+		const htmlArtists = [];
+		for(let i = 0; i<artists.length ; i++){
+			let artist = artists[i]
+			htmlArtists.push(<li>{artist._id} - {artist.name} </li>)
+		}
+		this.setState({htmlArtists: htmlArtists})
+		return (htmlArtists.length>0 )? htmlArtists : 'une liste d\'artistes'; 
 	}
 
 	render() {
-		const artists = this.getFormattedArtistsFromState()
+		const artists = this.state.artists
 		return (
 	 	<div>
 			<div>TITLE</div>
 			<div>
-				<form onSubmit={this.getArtistFromSearch()} >
 					<input type="text" 	ref={(search) => this.search = search} className="searchbar" placeholder="Search..." />
-					<button type="submit">Search</button>
-				</form>
+					
+					<button onClick={() => this.getAll()} >
+						ALL
+					</button>
+					
+					<button onClick={() => this.getSearch() } > Search </button>
 			</div>
-			<div>{artists}</div>
+			{this.state.htmlArtists}
 			<div>TAGS</div>
 	  	</div>
 	  )  
