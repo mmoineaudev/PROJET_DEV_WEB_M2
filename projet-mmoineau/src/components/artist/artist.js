e20f259349e760229ac4c0c8cb90c8a6e017d1a9
Max M <maxime.moineau.dev@gmail.com>
2020-01-26 17:42:37 +0100

    Ajout panneau depliant artiste

--- projet-mmoineau/src/components/artist/artist.js
+++ projet-mmoineau/src/components/artist/artist.js
@@ -4,32 +4,49 @@ import {  ListItem } from '@material-ui/core';
 class Artist extends React.Component {
     constructor(props) {
       super(props);
-      console.log('Artist :' , props)
       this.state = {
         name:'Jean',
         genre: '',
-        jsonArtist: {}
+        jsonArtist: {},
+        details: ''
       }
     }
 
 componentDidMount(){
-    console.log('hello, I am an artist')
     this.setState({
         name:this.props.name,
         genre: this.props.genre,
         jsonArtist: this.props.jsonArtist
     })
-    console.log('I am an artist : ', this.props, this.state)
 }
 
 render(){
-  const display = <ListItem align-items="center" button="true" key={this.state.jsonArtist._id} id={this.state.jsonArtist._id} onClick={console.log('clic')}> {this.state.jsonArtist.name} 
+  const details = this.state.details
+  const display = <div onClick={() => { this.fillDetails() }}><ListItem align-items="center" button="true" key={this.state.jsonArtist._id} id={this.state.jsonArtist._id} > {this.state.jsonArtist.name} 
   { (this.state.jsonArtist.genres && this.state.jsonArtist.genres.length>0) ? <div className="sublistitem"> {this.state.jsonArtist.genres.join(", " )}
-</div> :''} </ListItem>
+  </div> :''} </ListItem> 
+  {(details) ? details : '' }
+  </div>
     //console.log(display)
     return ( display )
 
-}
-
+  }
+  /*
+        {(this.state.jsonArtist.type) ? }
+      {(this.state.jsonArtist.albums) ?  .title .genre .dateRelease : ''}
+  */
+  fillDetails(){
+    let details = '';
+    if(this.state.details===''){
+      details = <div class="details"> 
+      {(this.state.jsonArtist.locationInfo) ? this.state.jsonArtist.locationInfo : ''}
+      {(this.state.jsonArtist.members && this.state.jsonArtist.members.length>0) ? 
+        this.state.jsonArtist.members.map(e => {
+          return e.name + (e.instruments && e.instruments.length>0)? ' '+ e.instruments.join(', ') : ''
+        }) : ''}
+    </div>
+    }
+    this.setState({details:details})
+  }
   }
   export default Artist;
